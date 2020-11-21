1a0b1327d90d05c2953c5ff8f9f4d2c32024756e
usrt0022 <usrt0022@students.zhaw.ch>
2020-11-21 16:27:00 +0100

    Game finally works (except for collision and some bugs)

--- public/js/game.ts
+++ public/js/game.ts
@@ -6,6 +6,8 @@ class Snake {
     private xSpeed: number;
     private ySpeed: number;
     public moveTheSnake?: any;
+    private counter: number;
+    private total: number;
     private canvas;
     private ctx;
     constructor(opts: SnakeConstruction) {
@@ -15,6 +17,8 @@ class Snake {
         this.ySpeed = 0;
         this.height = 20;
         this.width = 20;
+        this.counter = 1;
+        this.total = this.positionX.length;
         this.canvas = <HTMLCanvasElement>document.getElementById("canvas");
         this.ctx = <CanvasRenderingContext2D>this.canvas.getContext("2d");
     }
@@ -24,39 +28,254 @@ class Snake {
     getPositionY() {
         return this.positionY[0];
     }
+    isMovingOnXAxis(): boolean {
+        for (let y: number = this.positionY.length - 1; y > 0; y--) {
+            if (this.positionY[y] === this.positionY[y - 1]) {
+                this.counter += 1;
+            } else {
+                this.counter = this.counter;
+            }
+        }
+        if (this.counter === this.positionY.length) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    isMovingOnYAxis(): boolean {
+        for (let x: number = this.positionX.length - 1; x > 0; x--) {
+            if (this.positionX[x] === this.positionX[x - 1]) {
+                this.counter += 1;
+            } else {
+                this.counter = this.counter;
+            }
+        }
+        if (this.counter === this.positionX.length) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    howManyValuesAreTheSame(position: number[]) {
+        this.counter = 0;
+        for (let i: number = 0; i < position.length - 1; i++) {
+            if (position[i] === position[i - 1]) {
+                this.counter += 1;
+            } else {
+                this.counter = this.counter;
+            }
+        }
+        return this.counter;
+    }
+    initiateTurn(firstPosition: number[], secondPosition: number[], speed: number, firstIndex: number, secondIndex: number) {
+        if (firstIndex === 1) {
+            firstPosition[firstIndex] = firstPosition[firstIndex];
+            console.log(`x: ${firstPosition}`);
+        } else {
+            firstPosition[firstIndex] += speed;
+            console.log(`x: ${firstPosition}`);
+        }
+        if (secondIndex === 1) {
+            if (secondPosition[secondIndex] < secondPosition[secondIndex - 1]) {
+                secondPosition[secondIndex] += speed;
+                console.log(`y: ${secondPosition}`);
+            } else {
+                secondPosition[secondIndex] = secondPosition[secondIndex];
+            }
+        } else if (secondIndex > 1 && secondPosition[secondIndex] < secondPosition[secondIndex - 1]) {
+            secondPosition[secondIndex] += speed;
+            console.log(`y: ${secondPosition}`);
+        } else {
+            secondPosition[secondIndex] = secondPosition[secondIndex];
+        }
+    }
+    moveWhenGoingStraight(position: number[], speed: number, index: number) {
+        if (position[index] < position[index - 1]) {
+            if (index === 1) {
+                console.log("Index is 1");
+                if (position[index] === position[index + 1]) {
+                    position[index] += speed;
+                    console.log(`x:: ${position}`);
+                } else {
+                    position[index] = position[index];
+                    console.log(`x: ${position}`);
+                }
+            } else {
+                position[index] += speed;
+                console.log(`x: ${position}`);
+            }
+        } else {
+            position[index] = position[index];
+            console.log(`x: ${position}`);
+        }
+    }
+    moveWhenInATurn(firstPosition: number[], secondPosition: number[], speed: number, firstIndex: number, identicalValues: number) {
+        console.log(`Identical: ${identicalValues}`);
+        if (firstPosition[1] - firstPosition[0] > 20 || firstPosition[1] - firstPosition[0] < -20) {
+            firstPosition[1] += speed;
+        }
+        if (firstIndex === 0 || firstIndex === 1) {
+            firstPosition[firstIndex] = firstPosition[firstIndex];
+            console.log(`x: ${firstPosition}`);
+        } else {
+            if (firstPosition[firstIndex] === firstPosition[firstIndex - 1]) {
+                console.log(`${firstIndex} is 20`);
+                firstPosition[firstIndex] = firstPosition[firstIndex];
+                console.log(`x: ${firstPosition}`);
+            } else if (firstPosition[firstIndex] - firstPosition[firstIndex - 1] > 20 || firstPosition[firstIndex] - firstPosition[firstIndex - 1] < -20) {
+                firstPosition[firstIndex] += speed;
+            } else {
+                firstPosition[firstIndex] = firstPosition[firstIndex];
+                console.log(`x: ${firstPosition}`);
+            }
+        }
+        if (secondPosition[1] - secondPosition[0] > 20 || secondPosition[1] - secondPosition[0] < -20) {
+            secondPosition[1] += speed;
+        }
+        if (firstIndex === 0 || firstIndex === 1) {
+            secondPosition[firstIndex] = secondPosition[firstIndex];
+            console.log(`y: ${secondPosition}`);
+        } else {
+            if (identicalValues === firstIndex) {
+                secondPosition[firstIndex] = secondPosition[firstIndex];
+            } else {
+                if (secondPosition[firstIndex] === secondPosition[firstIndex - 1]) {
+                    secondPosition[firstIndex] = secondPosition[firstIndex];
+                } else {
+                    secondPosition[firstIndex] += speed;
+                }
+            }
+        }
+    }
     update(): void {
         this.ctx.fillStyle = "Black";
+        for (let index: number = this.positionX.length - 1; index >= 0; index--) {
+            this.ctx.clearRect(this.positionX[index], this.positionY[index], this.width, this.height);
+        }
         this.positionX[0] += this.xSpeed;
         this.positionY[0] += this.ySpeed;
-        if (this.positionX.length < 2 && this.positionY.length < 2) {
-            this.ctx.clearRect(this.positionX[0] - this.xSpeed, this.positionY[0] - this.ySpeed, this.width, this.height);
+        if (this.positionX.length < 2) {
             this.ctx.fillRect(this.positionX[0], this.positionY[0], this.height, this.width);
+            console.log(`x: ${this.positionX}
+            y: ${this.positionY}`);
+        } else if (this.positionX.length < 3) {
+            for (let index: number = this.positionX.length - 1; index >= 0; index--) {
+                if (index > 0) {
+                    this.positionX[index] = this.positionX[index - 1] - xSpeed;
+                    this.positionY[index] = this.positionY[index - 1] - ySpeed;
+                    this.ctx.fillRect(this.positionX[index], this.positionY[index], this.height, this.width);
+                } else {
+                    this.ctx.fillRect(this.positionX[index], this.positionY[index], this.height, this.width);
+                }
+            }
+            console.log(`Is < 3
+            x: ${this.positionX}
+            y: ${this.positionY}`);
         } else {
-            this.ctx.clearRect(this.positionX[0] - this.xSpeed, this.positionY[0] - this.ySpeed, this.width, this.height);
-            this.ctx.fillRect(this.positionX[0], this.positionY[0], this.height, this.width);
-            for (let x: number = this.positionX.length - 1; x > 0; x--) {
-                for (let y: number = this.positionY.length - 1; y > 0; y--) {
-                    if (x > 1 || y > 1) {
-                        this.positionX[x] = this.positionX[x - 1];                                                     // I don't know why, but I have to subtract the speed
-                        this.positionY[y] = this.positionY[y - 1];                                                     // in order for it to work correctly. But hey, problems that don't make sense, require solutions that don't make sense.
-                        this.ctx.clearRect(this.positionX[x - 1], this.positionY[y - 1], this.width, this.height);     // And for some reason, clearRect doesn't work for the tail.
-                        this.ctx.fillRect(this.positionX[x], this.positionY[y], this.height, this.width);
-                    } else if (this.xSpeed != 0) {
-                        this.positionX[x] = this.positionX[0] - this.xSpeed;
-                        this.positionY[y] = this.positionY[0];
-                        this.ctx.clearRect(this.positionX[x], this.positionY[y], this.width, this.height);
-                        this.ctx.fillRect(this.positionX[x], this.positionY[y], this.height, this.width);
-                    } else if (this.ySpeed != 0) {
-                        this.positionX[x] = this.positionX[0];
-                        this.positionY[y] = this.positionY[0] - this.ySpeed;
-                        this.ctx.clearRect(this.positionX[x - 1], this.positionY[y - 1], this.width, this.height);
-                        this.ctx.fillRect(this.positionX[x], this.positionY[y], this.height, this.width);
-                    }
+            for (let index: number = this.positionX.length - 1; index >= 0; index--) {
+                if (index === 1) {
+                    this.positionX[index] = this.positionX[index - 1] - xSpeed;
+                    this.positionY[index] = this.positionY[index - 1] - ySpeed;
+                    this.ctx.fillRect(this.positionX[index], this.positionY[index], this.height, this.width);
+                } else if (index > 1) {
+                    this.positionX[index] = this.positionX[index - 1];
+                    this.positionY[index] = this.positionY[index - 1];
+                    this.ctx.fillRect(this.positionX[index], this.positionY[index], this.height, this.width);
+                }
+                else {
+                    // this.positionX[index] = this.positionX[index - 1];
+                    // this.positionY[index] = this.positionY[index - 1];
+                    this.ctx.fillRect(this.positionX[index], this.positionY[index], this.height, this.width);
                 }
             }
             console.log(`x: ${this.positionX}
-                    y: ${this.positionY}`);
+            y: ${this.positionY}`);
         }
+
+        // if (this.isMovingOnYAxis()) {
+        //     console.log("Is moving on y Axis");
+        //     this.positionX[0] += this.xSpeed;
+        //     this.positionY[0] += this.ySpeed;
+        //     for (let y: number = this.positionY.length - 1; y > 0; y--) {
+        //         for (let x: number = this.positionX.length - 1; x > 0; x--) {
+        //             if (this.positionX[x - 1] > this.positionX[x]) {
+        //                 this.initiateTurn(this.positionX, this.positionY, this.xSpeed, x, y);
+        //                 this.ctx.clearRect(this.positionX[0] - this.xSpeed, this.positionY[0] - this.ySpeed, this.width, this.height);
+        //                 this.ctx.fillRect(this.positionX[0], this.positionY[0], this.height, this.width);
+        //             } else {
+        //                 this.moveWhenGoingStraight(this.positionY, this.ySpeed, y);
+        //                 this.ctx.clearRect(this.positionX[0] - this.xSpeed, this.positionY[0] - this.ySpeed, this.width, this.height);
+        //                 this.ctx.fillRect(this.positionX[0], this.positionY[0], this.height, this.width);
+        //             }
+        //         }
+        //     }
+        // } else if (this.isMovingOnXAxis()) {
+        //     console.log("Is moving on x Axis");
+        //     this.positionX[0] += this.xSpeed;
+        //     this.positionY[0] += this.ySpeed;
+        //     for (let x: number = this.positionX.length - 1; x > 0; x--) {
+        //         for (let y: number = this.positionY.length - 1; y > 0; y--) {
+        //             if (this.positionY[y - 1] > this.positionY[y]) {
+        //                 this.initiateTurn(this.positionY, this.positionX, this.ySpeed, y, x);
+        //                 this.ctx.clearRect(this.positionX[0] - this.xSpeed, this.positionY[0] - this.ySpeed, this.width, this.height);
+        //                 this.ctx.fillRect(this.positionX[0], this.positionY[0], this.height, this.width);
+        //             } else {
+        //                 this.moveWhenGoingStraight(this.positionX, this.xSpeed, x);
+        //                 this.ctx.clearRect(this.positionX[0] - this.xSpeed, this.positionY[0] - this.ySpeed, this.width, this.height);
+        //                 this.ctx.fillRect(this.positionX[0], this.positionY[0], this.height, this.width);
+        //             }
+        //         }
+        //     }
+        // } else {
+        //     console.log("Is in a curve");
+        //     this.positionX[0] += this.xSpeed;
+        //     this.positionY[0] += this.ySpeed;
+        //     const identicalValuesX: number = this.howManyValuesAreTheSame(this.positionX);
+        //     const identicalValuesY: number = this.howManyValuesAreTheSame(this.positionY);
+        //     for (let x: number = this.positionX.length - 1; x > 0; x--) {
+        //         if (this.xSpeed != 0) {
+        //             this.moveWhenInATurn(this.positionX, this.positionY, this.xSpeed, x, identicalValuesY);
+        //             this.ctx.clearRect(this.positionX[0] - this.xSpeed, this.positionY[0] - this.ySpeed, this.width, this.height);
+        //             this.ctx.fillRect(this.positionX[0], this.positionY[0], this.height, this.width);
+        //         } else {
+        //             this.moveWhenInATurn(this.positionY, this.positionX, this.ySpeed, x, identicalValuesX);
+        //             this.ctx.clearRect(this.positionX[0] - this.xSpeed, this.positionY[0] - this.ySpeed, this.width, this.height);
+        //             this.ctx.fillRect(this.positionX[0], this.positionY[0], this.height, this.width);
+        //         }
+        //     }
+        // }
+        // this.ctx.fillStyle = "Black";
+        // this.positionX[0] += this.xSpeed;
+        // this.positionY[0] += this.ySpeed;
+        // if (this.positionX.length < 2 && this.positionY.length < 2) {
+        //     this.ctx.clearRect(this.positionX[0] - this.xSpeed, this.positionY[0] - this.ySpeed, this.width, this.height);
+        //     this.ctx.fillRect(this.positionX[0], this.positionY[0], this.height, this.width);
+        // } else {
+        //     this.ctx.clearRect(this.positionX[0] - this.xSpeed, this.positionY[0] - this.ySpeed, this.width, this.height);
+        //     this.ctx.fillRect(this.positionX[0], this.positionY[0], this.height, this.width);
+        //     for (let x: number = this.positionX.length - 1; x > 0; x--) {
+        //         for (let y: number = this.positionY.length - 1; y > 0; y--) {
+        //             if (x > 1 || y > 1) {
+        //                 this.positionX[x] = this.positionX[x - 1];                                                     // I don't know why, but I have to subtract the speed
+        //                 this.positionY[y] = this.positionY[y - 1];                                                     // in order for it to work correctly. But hey, problems that don't make sense, require solutions that don't make sense.
+        //                 this.ctx.clearRect(this.positionX[x - 1], this.positionY[y - 1], this.width, this.height);     // And for some reason, clearRect doesn't work for the tail.
+        //                 this.ctx.fillRect(this.positionX[x], this.positionY[y], this.height, this.width);
+        //             } else if (this.xSpeed != 0) {
+        //                 this.positionX[x] = this.positionX[0] - this.xSpeed;
+        //                 this.positionY[y] = this.positionY[0];
+        //                 this.ctx.clearRect(this.positionX[x], this.positionY[y], this.width, this.height);
+        //                 this.ctx.fillRect(this.positionX[x], this.positionY[y], this.height, this.width);
+        //             } else if (this.ySpeed != 0) {
+        //                 this.positionX[x] = this.positionX[0];
+        //                 this.positionY[y] = this.positionY[0] - this.ySpeed;
+        //                 this.ctx.clearRect(this.positionX[x - 1], this.positionY[y - 1], this.width, this.height);
+        //                 this.ctx.fillRect(this.positionX[x], this.positionY[y], this.height, this.width);
+        //             }
+        //         }
+        //     }
+        //     console.log(`x: ${this.positionX}
+        //             y: ${this.positionY}`);
+        // }
     }
     move(xSpeed: number, ySpeed: number) {
         this.xSpeed = xSpeed;
@@ -66,6 +285,8 @@ class Snake {
     eat(): void {
         this.positionX.push(this.positionX[this.positionX.length - 1] - this.xSpeed);
         this.positionY.push(this.positionY[this.positionY.length - 1] - this.ySpeed);
+        console.log(`Snake ate: x: ${this.positionX}
+        y: ${this.positionY}`);
     }
 }
 
--- public/js/test.ts
+++ public/js/test.ts
@@ -1,5 +1,5 @@
-let xPosition: number[] = [80, 80, 80, 80, 80];
-let yPosition: number[] = [160, 140, 120, 100, 80];
+let xPosition: number[] = [80, 80];
+let yPosition: number[] = [160, 140];
 
 const isMovingOnXAxis = (): boolean => {
     let counter: number = 1;
@@ -31,6 +31,18 @@ const isMovingOnYAxis = (): boolean => {
     } else {
         return false;
     }
+};
+
+const howManyValuesAreTheSame = (position: number[]) => {
+    let counter: number = 0;
+    for (let i: number = 0; i < position.length - 1; i++) {
+        if (position[i] === position[i - 1]) {
+            counter += 1;
+        } else {
+            counter = counter;
+        }
+    }
+    return counter;
 };
 
 const initiateTurn = (firstPosition: number[], secondPosition: number[], speed: number, firstIndex: number, secondIndex: number) => {
@@ -77,16 +89,17 @@ const moveWhenGoingStraight = (position: number[], speed: number, index: number)
     }
 };
 
-const moveWhenInATurn = (firstPosition: number[], secondPosition: number[], speed: number, firstIndex: number, secondIndex: number) => {
+const moveWhenInATurn = (firstPosition: number[], secondPosition: number[], speed: number, firstIndex: number, identicalValues: number) => {
+    console.log(`Identical: ${identicalValues}`);
     if (firstPosition[1] - firstPosition[0] > 20 || firstPosition[1] - firstPosition[0] < -20) {
         firstPosition[1] += speed;
     }
-    if (firstIndex === 0) {
+    if (firstIndex === 0 || firstIndex === 1) {
         firstPosition[firstIndex] = firstPosition[firstIndex];
         console.log(`x: ${firstPosition}`);
     } else {
         if (firstPosition[firstIndex] === firstPosition[firstIndex - 1]) {
-            console.debug(`${firstIndex} is 20`);
+            console.log(`${firstIndex} is 20`);
             firstPosition[firstIndex] = firstPosition[firstIndex];
             console.log(`x: ${firstPosition}`);
         } else if (firstPosition[firstIndex] - firstPosition[firstIndex - 1] > 20 || firstPosition[firstIndex] - firstPosition[firstIndex - 1] < -20) {
@@ -99,20 +112,33 @@ const moveWhenInATurn = (firstPosition: number[], secondPosition: number[], spee
     if (secondPosition[1] - secondPosition[0] > 20 || secondPosition[1] - secondPosition[0] < -20) {
         secondPosition[1] += speed;
     }
-    if (secondIndex === 0) {
-        secondPosition[secondIndex] = secondPosition[secondIndex];
+    if (firstIndex === 0 || firstIndex === 1) {
+        secondPosition[firstIndex] = secondPosition[firstIndex];
         console.log(`y: ${secondPosition}`);
     } else {
-        if (secondPosition[secondIndex] === secondPosition[secondIndex - 1]) {
-            console.debug(`${secondIndex} is 20`);
-            secondPosition[secondIndex] = secondPosition[secondIndex];
-            console.log(`y: ${secondPosition}`);
-        } else if (secondPosition[secondIndex] - secondPosition[secondIndex - 1] >= 20 || secondPosition[secondIndex] - secondPosition[secondIndex - 1] <= -20) {
-            secondPosition[secondIndex] += speed;
+        if (identicalValues === firstIndex) {
+            secondPosition[firstIndex] = secondPosition[firstIndex];
         } else {
-            secondPosition[secondIndex] = secondPosition[secondIndex];
-            console.log(`y: ${secondPosition}`);
+            /*let newIdenticalValues: number = howManyValuesAreTheSame(secondPosition);
+            if (newIdenticalValues > identicalValues) {
+                return;
+            }*/
+            if (secondPosition[firstIndex] === secondPosition[firstIndex - 1]) {
+                secondPosition[firstIndex] = secondPosition[firstIndex];
+            } else {
+                secondPosition[firstIndex] += speed;
+            }
         }
+        /*if (secondPosition[firstIndex] === secondPosition[firstIndex - 1]) {
+            console.debug(`${firstIndex} is 20`);
+            secondPosition[firstIndex] = secondPosition[firstIndex];
+            console.log(`y: ${secondPosition}`);
+        } else if (secondPosition[firstIndex - 1] === secondPosition[firstIndex - 2]) {
+            secondPosition[firstIndex] += speed;
+        } else {
+            secondPosition[firstIndex] = secondPosition[firstIndex];
+            console.log(`y: ${secondPosition}`);
+        }*/
     }
 };
 
@@ -147,19 +173,19 @@ const move = (xSpeed: number, ySpeed: number) => {
         console.log("Is in a curve");
         xPosition[0] += xSpeed;
         yPosition[0] += ySpeed;
+        const identicalValuesX: number = howManyValuesAreTheSame(xPosition);
+        const identicalValuesY: number = howManyValuesAreTheSame(yPosition);
         for (let x: number = xPosition.length - 1; x > 0; x--) {
-            for (let y: number = yPosition.length - 1; y > 0; y--) {
-                if (xSpeed != 0) {
-                    moveWhenInATurn(xPosition, yPosition, xSpeed, x, y);
-                } else {
-                    moveWhenInATurn(yPosition, xPosition, ySpeed, y, x);
-                }
+            if (xSpeed != 0) {
+                moveWhenInATurn(xPosition, yPosition, xSpeed, x, identicalValuesY);
+            } else {
+                moveWhenInATurn(yPosition, xPosition, ySpeed, x, identicalValuesX);
             }
         }
     }
 };
 
-for (let i: number = 1; i <= 3; i++) {
+for (let i: number = 1; i <= 2; i++) {
     console.log(`${i}. Feld`);
     move(20, 0);
 }
