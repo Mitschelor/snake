30d3cbc5a4820e37f3fbf76a7420e46ffb4f1ce3
usrt0022 <usrt0022@students.zhaw.ch>
2020-11-30 20:36:03 +0100

    change

--- src/controllers/db_connector.ts
+++ src/controllers/db_connector.ts
@@ -8,6 +8,5 @@ const uri: any = process.env.DB_URI;
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-    .then(() => console.log("success"))
-    .catch((error) => console.log(error));
+}).catch((error) => console.log(error));
+
