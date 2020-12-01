49be9bec5f7b329909f0610c1ca673e56514a3fe
usrt0022 <usrt0022@students.zhaw.ch>
2020-12-01 20:29:33 +0100

    change branche

--- objects/database.ts
+++ objects/database.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { UserData } from "../models/user_data";
+import { Scores } from "../models/scores";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -24,11 +25,35 @@ export class DataBase {
         this.password = userData.password;
     }
 
-    connect() {
+    private connect() {
         mongoose.connect(this.uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         }).catch((error) => console.log(error));
+    }
+
+    saveScore() {
+        this.connect();
+        const instance = new Scores({
+            userId: this.userId,
+            score: this.score
+        });
+
+        instance.save().catch((error) => console.log(error));
+    }
+
+    saveUser() {
+        this.connect();
+        const instance = new UserData({
+            userId: this.userId,
+            firstname: this.firstName,
+            lastname: this.lastName,
+            username: this.userName,
+            email: this.email,
+            password: this.password
+        });
+
+        instance.save().catch((error) => console.log(error));
     }
 }
 
